c266e2985adccee1c6d2df2da4915f81f5f4f556
cristi40 <ccristescu40@gmail.com>
2023-06-18 16:40:41 +0300

    more repos

--- src/main/java/com/example/booo/Genre/GenreRepository.java
+++ src/main/java/com/example/booo/Genre/GenreRepository.java
@@ -4,7 +4,22 @@ import com.example.booo.BookGenres.BookGenresEntity;
 import com.example.booo.DataBase.DataBase;
 import com.example.booo.Repository;
 
+import javax.xml.crypto.Data;
+import java.util.List;
+
 public abstract class GenreRepository implements Repository<GenreEntity, Long> {
+
+    @Override
+    public GenreEntity findByID(Long aLong) {
+        var query = DataBase.getInstance().createNamedQuery("genres.findById", GenreEntity.class);
+        query.setParameter(1, aLong);
+        return query.getSingleResult();
+    }
+    @Override
+    public List<GenreEntity> findAll() {
+        return DataBase.getInstance().createNamedQuery("genres.findAll", GenreEntity.class)
+                .getResultList();
+    }
     @Override
     public void deleteByID(Long aLong) {
         DataBase.getInstance().getTransaction().begin();
--- src/main/java/com/example/booo/Group/GroupRepository.java
+++ src/main/java/com/example/booo/Group/GroupRepository.java
@@ -4,7 +4,15 @@ import com.example.booo.BookGenres.BookGenresEntity;
 import com.example.booo.DataBase.DataBase;
 import com.example.booo.Repository;
 
+import javax.xml.crypto.Data;
+
 public abstract class GroupRepository implements Repository<GroupEntity, Long> {
+    @Override
+    public GroupEntity findByID(Long aLong) {
+        return DataBase.getInstance().createNamedQuery("groups.findById", GroupEntity.class)
+                .setParameter(1, aLong)
+                .getSingleResult();
+    }
     @Override
     public void deleteByID(Long aLong) {
         DataBase.getInstance().getTransaction().begin();
