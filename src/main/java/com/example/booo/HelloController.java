b7cb7d73de4df39cb72a72c94279cabf5da51eb0
Marcel <timofte.2002@gmail.com>
2023-06-18 17:35:06 +0300

    aoleo controller

--- src/main/java/com/example/booo/HelloController.java
+++ src/main/java/com/example/booo/HelloController.java
@@ -5,34 +5,27 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "HelloController", value = "/api/hello")
+@WebServlet(urlPatterns = {"/hello"})
 public class HelloController extends HttpServlet {
-    @Override
-    public void init() {
-
-    }
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/plain");
-
+        response.setStatus(200);
         PrintWriter out = response.getWriter();
-        out.println("hello world");
+        if(request.getRequestURI().equals("/hello")){
+            out.println("hello world");
+        }else{
+            out.println(request.getRequestURI().split("/")[1]);
+        }
         out.close();
-//        out.println("<h1>Hello</h1>");
-//        out.println("</body></html>");
-
     }
 
-//    @Override
-//    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        PrintWriter out = response.getWriter();
-//        out.println("Resource Created");
-//        response.setStatus(200);
-//        out.close();
-//    }
-
     @Override
-    public void destroy() {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        response.setStatus(200);
+        PrintWriter out = response.getWriter();
+        out.println("Resource Created");
+        out.close();
     }
 }
